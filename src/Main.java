54a1bbf93e9f5ccb4dbce5524f90464abc42f8ba
Huzaifa123451 <huzaifaahmer978@gmail.com>
2025-10-08 17:09:25 -0400

    Called the main file to make the project functional

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+package finsight;
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            PortfolioManager portfolioManager = new PortfolioManager();
+            new FinSightGUI(portfolioManager);
+        });
+    }
+}
